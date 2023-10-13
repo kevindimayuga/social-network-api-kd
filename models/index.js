6e3e36a61cdf844bfa1797a4d7b9738a9f5705fc
kevindimayuga <k.dimayuga22@gmail.com>
2023-10-13 17:27:17 -0500

    finished models index.js

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// This will import the User model and the Thought model
+const User = require('./User');
+const Thought = require('./Thought');
+
+// This will export the User model and the Thought model
+module.exports = { User, Thought };
