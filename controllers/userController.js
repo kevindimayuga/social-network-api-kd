98e771a992959d3fa71fc13a2d8f826e776a2258
kevindimayuga <k.dimayuga22@gmail.com>
2023-10-16 18:27:22 -0500

    updates to userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,21 +12,56 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
-    }
+    },
+
     // This will GET a single user by their '_id' and populated thought and friend data
     async getSingleUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId })
                 .select('-__v')
-        if (!user) {
-            return res.status(404).json({ message: 'No user found with that ID' });
-        }
-        res.json(user);
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that ID' });
+            }
+            res.json(user);
         } catch (err) {
             res.status(500).json(err);
         }
-    }
+    },
+
+    // This will POST a new user
     async createUser(req, res) {
-        
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // This will update (PUT) a user by their '_id'
+    async updateUser(req, res) {
+        // This will find a user by their '_id' and update their data
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                req.body,
+                { new: true }
+            )
+            // If no user is found, this will send the 404 error message
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that ID' });
+            }
+            // Otherwise, this will send the updated user data
+            res.json(user);
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
     }
+
+    // This will DELETE a user by their '_id'
+
+    // This will add a friend for a user by their '_id'
+
+    // This will remove a friend from a user by their '_id'
+    
 }
